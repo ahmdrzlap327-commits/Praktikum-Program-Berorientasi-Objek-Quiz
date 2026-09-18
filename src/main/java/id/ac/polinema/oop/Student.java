57134ff72046bf93d52a751db2350a54da017e59
AliPasha <ahmdrzlap327@gmail.com>
2026-09-18 08:26:41 +0700

    coba 4

--- src/main/java/id/ac/polinema/oop/Student.java
+++ src/main/java/id/ac/polinema/oop/Student.java
@@ -54,8 +54,8 @@ public class Student {
     public String getPredicate() {
         // TODO: implement the predicate logic per the rules above
         if (gpa >= 3.5) {
-            return "Cum laude";
-        } else if (gpa <= 3.0 && gpa < 3.5) {
+            return "Cum Laude";
+        } else if (gpa >= 3.0 && gpa < 3.5) {
             return "Very Satisfactory";
         } else {
             return "Satisfactory";
